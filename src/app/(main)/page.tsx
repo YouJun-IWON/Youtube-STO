616ceb2bd45e1a59a8e29041ffd9ea8ad1f5f8b4
ReasonJun <117330708+YouJun-IWON@users.noreply.github.com>
2024-01-07 17:50:46 +0900

    fix: :bug: fix router error

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,19 @@
+import Feature from './_components/Feature';
+import Fourth from './_components/Fourth';
+import Hero from './_components/Hero';
+import Partners from './_components/Partners';
+import Second from './_components/Second';
+import Third from './_components/Third';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Partners />
+      <Second />
+      <Third />
+      <Feature />
+      <Fourth />
+    </>
+  );
+}
